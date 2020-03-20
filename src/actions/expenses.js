f29684670d3511188c1f46ddc53410dc521bf80d
Chris Young <cyoung@power-pole.com>
2020-03-20 11:01:50 -0400

    setup devDependencies and dist/ folder

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,33 +1,33 @@
 import uuid from 'uuid';
 
-//ADD_EXPENSE
+// ADD_EXPENSE
 export const addExpense = (
-    { description = 'Rent',
+    {
+        description = '',
         note = '',
-        amount = 109500,
+        amount = 0,
         createdAt = 0
-    } = {}) => ({
-        type: 'ADD_EXPENSE',
-        expense: {
-            id: uuid(),
-            description,
-            note,
-            amount,
-            createdAt
-        }
-    });
-
-//REMOVE_EXPENSE
-export const removeExpense = () => ({
-    type: 'REMOVE_EXPENSE',
-    id: '123abc'
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
 });
 
-//EDIT_EXPENSE
+// REMOVE_EXPENSE
+export const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
+// EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
-    id: '123abcd',
-    updates: {
-        note: 'New note value'
-    }
+    id,
+    updates
 });
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,13 +1,15 @@
 import moment from 'moment';
 
-export default getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+// Get visible expenses
+
+export default (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
         const createdAtMoment = moment(expense.createdAt);
         const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
         const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
-        return startDate && endDate && textMatch;
+        return startDateMatch && endDateMatch && textMatch;
     }).sort((a, b) => {
         if (sortBy === 'date') {
             return a.createdAt < b.createdAt ? 1 : -1;
